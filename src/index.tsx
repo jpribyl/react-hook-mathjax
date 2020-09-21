6bdb376044113d07e002c2c9d644cefc15839449
jpribyl <pribylsnbits@gmail.com>
2020-09-21 12:46:40 -0600

    Add onSuccess handler

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useContext, useEffect, useRef, useState } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 declare global {
   interface Window {
@@ -20,9 +26,9 @@ export const MathJaxProvider: React.FC<ContextProps> = ({
 
   useEffect(() => {
     if (!window.MathJax) {
-      const script = document.createElement('script');
+      const script = document.createElement("script");
       window.MathJax = mathJax;
-      script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+      script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js";
       script.async = true;
       script.onload = () => setMathJax(window.MathJax);
       ref.current?.appendChild(script);
@@ -42,24 +48,34 @@ export const MathJaxProvider: React.FC<ContextProps> = ({
   );
 };
 
-type Tex2SVGProps = any & { latex: string; onError?: () => void };
-const Tex2SVG: React.FC<Tex2SVGProps> = ({ latex = '', onError = () => {}, ...props }) => {
+type Tex2SVGProps = any & {
+  latex: string;
+  onError?: () => void;
+  onSuccess?: () => void;
+};
+const Tex2SVG: React.FC<Tex2SVGProps> = ({
+  latex = "",
+  onError = () => {},
+  onSuccess = () => {},
+  ...props
+}) => {
   const mathJax = useContext(MathJaxContext) || null;
   const [html, setHtml] = useState<HTMLElement | null>(null);
   const ref = useRef<HTMLElement | null>(null);
   const hasError = !!html?.outerHTML.match(/data-mjx-error/);
 
   useEffect(() => {
-    if (hasError) onError();
-  }, [hasError, onError]);
-
-  useEffect(() => {
     setHtml((mathJax as any | null)?.tex2svg?.(latex));
   }, [mathJax, latex]);
 
   useEffect(() => {
+    if (html && hasError) onError();
+    if (html && !hasError) onSuccess();
+  }, [html]);
+
+  useEffect(() => {
     if (html) {
-      Object.keys(props).map(key => html.setAttribute(key, props[key]));
+      Object.keys(props).map((key) => html.setAttribute(key, props[key]));
       ref.current?.appendChild(html);
       return () => {
         ref.current?.removeChild(html);
@@ -70,10 +86,11 @@ const Tex2SVG: React.FC<Tex2SVGProps> = ({ latex = '', onError = () => {}, ...pr
   }, [props, html]);
 
   if (!mathJax) {
-    console.error('MathJax is not available! Are you using <Tex2SVG> outside of <MathJaxContext>?')
-    return null
+    console.error(
+      "MathJax is not available! Are you using <Tex2SVG> outside of <MathJaxContext>?",
+    );
+    return null;
   }
-
 
   return <span ref={ref} />;
 };
