184030dadd3ee1fc866d14da491f1a5db5ccac1a
jpribyl <pribylsnbits@gmail.com>
2020-09-21 17:06:12 -0600

    Updates

---
+++ precache-manifest.0a1038239cedc2557b7368f3c0581954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fa4b5c912f09b782615262337a110a",
+    "url": "/react-hook-mathjax/index.html"
+  },
+  {
+    "revision": "6caeed1101df009197f2",
+    "url": "/react-hook-mathjax/static/css/main.77ed7596.chunk.css"
+  },
+  {
+    "revision": "faccc5530b2970d2e4a8",
+    "url": "/react-hook-mathjax/static/js/2.501e6ec6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hook-mathjax/static/js/2.501e6ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6caeed1101df009197f2",
+    "url": "/react-hook-mathjax/static/js/main.44ec225d.chunk.js"
+  },
+  {
+    "revision": "9459377bf189154d37d7",
+    "url": "/react-hook-mathjax/static/js/runtime-main.34f0f7ad.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-hook-mathjax/precache-manifest.0a1038239cedc2557b7368f3c0581954.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-hook-mathjax/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
